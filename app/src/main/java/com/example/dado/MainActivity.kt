3dafdd6ad59fe5b97d213e7d4e21bfc5ffa45eba
ricard <ricardogarcia200428>
2022-10-17 21:41:03 -0400

    sexo

---
+++ app/src/main/java/com/example/dado/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.dado
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.graphics.drawable.toDrawable
+import com.example.dado.databinding.ActivityMainBinding
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+        dado()
+    }
+
+    fun dado() {
+
+        binding.tvBoton.setOnClickListener {
+            when (Random.nextInt(6)) {
+                1 -> {
+                    binding.ivDado.setImageResource(R.drawable.dice_1)
+                }
+                2 -> {
+                    binding.ivDado.setImageResource(R.drawable.dice_2)
+                }
+                3 -> {
+                    binding.ivDado.setImageResource(R.drawable.dice_3)
+
+                }
+                4 -> {
+                    binding.ivDado.setImageResource(R.drawable.dice_4)
+
+                }
+                5 -> {
+                    binding.ivDado.setImageResource(R.drawable.dice_5)
+                }
+                else -> {
+                    binding.ivDado.setImageResource(R.drawable.dice_6)
+
+                }
+            }
+        }
+    }
+}
