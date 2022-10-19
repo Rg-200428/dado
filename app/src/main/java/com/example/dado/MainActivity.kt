0b06e5140683d89671bbbad86692605ea29c3298
ricard <ricardogarcia200428>
2022-10-19 14:24:35 -0400

    dado

--- app/src/main/java/com/example/dado/MainActivity.kt
+++ app/src/main/java/com/example/dado/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.dado
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.graphics.drawable.toDrawable
 import com.example.dado.databinding.ActivityMainBinding
@@ -14,38 +15,42 @@ class MainActivity : AppCompatActivity() {
 
         setContentView(binding.root)
         dado()
+        segundoDado()
     }
 
     fun dado() {
 
-        binding.tvBoton.setOnClickListener {
+        binding.tvTirarDado.setOnClickListener {
             when (Random.nextInt(6)) {
-                1 -> {
-                    binding.ivDado.setImageResource(R.drawable.dice_1)
-                }
-                2 -> {
-                    binding.ivDado.setImageResource(R.drawable.dice_2)
-                }
-                3 -> {
-                    binding.ivDado.setImageResource(R.drawable.dice_3)
-
-                }
-                4 -> {
-                    binding.ivDado.setImageResource(R.drawable.dice_4)
-
-                }
-                5 -> {
-                    binding.ivDado.setImageResource(R.drawable.dice_5)
-                }
-                else -> {
-                    binding.ivDado.setImageResource(R.drawable.dice_6)
-
-                }
+                1 -> binding.ivDadoImage.setImageResource(R.drawable.dice_1)
+                2 -> binding.ivDadoImage.setImageResource(R.drawable.dice_2)
+                3 -> binding.ivDadoImage.setImageResource(R.drawable.dice_3)
+                4 -> binding.ivDadoImage.setImageResource(R.drawable.dice_4)
+                5 -> binding.ivDadoImage.setImageResource(R.drawable.dice_5)
+                else -> binding.ivDadoImage.setImageResource(R.drawable.dice_6)
+            }
+            when (Random.nextInt(6)) {
+                1 -> binding.ivDadoImage2.setImageResource(R.drawable.dice_1)
+                2 -> binding.ivDadoImage2.setImageResource(R.drawable.dice_2)
+                3 -> binding.ivDadoImage2.setImageResource(R.drawable.dice_3)
+                4 -> binding.ivDadoImage2.setImageResource(R.drawable.dice_4)
+                5 -> binding.ivDadoImage2.setImageResource(R.drawable.dice_5)
+                else -> binding.ivDadoImage2.setImageResource(R.drawable.dice_6)
             }
         }
     }
 
-    fun segundoDadoSoproPAto() {
-        //Sopro guei y marica
+    fun segundoDado() {
+        binding.tvOtroDado.setOnClickListener() {
+                binding.ivDadoImage2.setVisibility(View.VISIBLE)
+                binding.tvQuitar.setVisibility(View.VISIBLE)
+                binding.tvOtroDado.setVisibility(View.INVISIBLE)
+            }
+        binding.tvQuitar.setOnClickListener(){
+            binding.ivDadoImage2.setVisibility(View.INVISIBLE)
+            binding.tvQuitar.setVisibility(View.INVISIBLE)
+            binding.tvOtroDado.setVisibility(View.VISIBLE)
+
+     }
     }
 }
